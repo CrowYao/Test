377fe0b9d8d6375bb6f92643677e4a55fcd4e506
CrowYao <crowyao@outlook.com>
2023-06-26 22:33:57 +0800

    master method add

--- Test.java
+++ Test.java
@@ -2,5 +2,7 @@ public class Test{
     public static void main(String[] args) {
         System.out.println("hello");
     }
-
+    public static void master() {
+        System.out.println("master分支方法");
+    }
 }  
